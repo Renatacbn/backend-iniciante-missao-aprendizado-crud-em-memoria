57df5decbd62141af2b12884667d76563ba78bc0
Renata Nunes <134668464+Renatacbn@users.noreply.github.com>
2024-05-13 09:32:23 +0000

    Implementação do Creat '[POST] /personagem

--- index.js
+++ index.js
@@ -6,23 +6,43 @@ app.get('/', function (req, res) {
 })
 
 const lista = ['Java', 'Kotlin', 'Android']
-// emplementando Endpoint Read All (irá funcionar com GET)/ neste ponto pensar em qual será a entidade ex.personagem
+// Implementando Endpoint Read All (irá funcionar com GET)/ neste ponto pensar em qual será a entidade ex.personagem
 
-// função para exibir em um único endpoint de Read All toda a lista 
+// Função para exibir em um único endpoint de Read All toda a lista 
 app.get('/personagem', function (req, res){
   res.send(lista)
 })
 
-// implementando Endpoint Read Id [Get] /personagem/:id(os dois pontos id funcionam para dizer que é uma especificação direcionada )
+// Implementando Endpoint Read Id [Get] /personagem/:id(os dois pontos id funcionam para dizer que é uma especificação direcionada )
 app.get('/personagem/:id', function (req, res){
   // função de requisição de parâmetro de rota id
   const id = req.params.id
 
-  // função para acessar o item da lista usando ID -1
+  // Função para acessar o item da lista usando ID -1
   const item = lista[id -1]
   
   // função para enviar um item como resposta
   res.send(item)
 })
+
+// Função para sinalizar para o express que está sendo utilizado o Json no body
+app.use(express.json())
+
+// Implementando Endpoint Creat [POST] /personagem
+app.post('/personagem', function (req, res){
+ 
+  // Função para acessar o Body da requisição através do req.body
+ const body = req.body
+
+//  Função para adicionar um novo item na lista acessando a propriedade 'nome' do body
+const novoItem = body.nome
+
+// Adicionar na lista
+lista.push(novoItem)
+
+// Função para exibir uma mensagem de sucesso
+  res.send('Item adicionado com sucesso: ' + novoItem)
+})
+
 
 app.listen(3000)
