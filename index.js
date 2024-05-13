609b33ff298d50e9e87b1260d412861918f99a79
Renata Nunes <134668464+Renatacbn@users.noreply.github.com>
2024-05-13 02:59:02 +0000

    Endpoint 'Read All [Get] /personagem' implementado

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+const lista = ['Java', 'Kotlin', 'Android']
+// emplementando Endpoint Read All (irá funcionar com GET)/ neste ponto pensar em qual será a entidade ex.personagem
+
+// função para exibir em um único endpoint de Read All toda a lista 
+app.get('/personagem', function (req, res){
+  res.send(lista)
+})
+
+
 app.listen(3000)
