de3c06e0fc21024dc33e1f3e4eb17ab653209fb3
Renata Nunes <134668464+Renatacbn@users.noreply.github.com>
2024-05-14 12:26:57 +0000

    Implementação do Endpoint '[DELETE] /personagem/:id

--- index.js
+++ index.js
@@ -10,18 +10,19 @@ const lista = ['Java', 'Kotlin', 'Android']
 
 // Função para exibir em um único endpoint de Read All toda a lista 
 app.get('/personagem', function (req, res){
-  res.send(lista)
+  // Função do 'filter.boolean' é para que os espaços quando deletar, não aparecer em vizualização como 'null' 
+  res.send(lista.filter(Boolean))
 })
 
 // Implementando Endpoint Read Id [Get](obter informação) /personagem/:id(os dois pontos id funcionam para dizer que é uma especificação direcionada )
 app.get('/personagem/:id', function (req, res){
-  // função de requisição de parâmetro de rota id
-  const id = req.params.id
+// função de requisição de parâmetro de rota id
+const id = req.params.id
 
-  // Função para acessar o item da lista usando ID -1
-  const item = lista[id -1]
+// Função para acessar o item da lista usando ID -1
+const item = lista[id -1]
   
-  // função para enviar um item como resposta
+// função para enviar um item como resposta
   res.send(item)
 })
 
@@ -31,8 +32,8 @@ app.use(express.json())
 // Implementando Endpoint Creat [POST](criar informação) /personagem
 app.post('/personagem', function (req, res){
  
-  // Função para acessar o Body da requisição através do req.body
- const body = req.body
+// Função para acessar o Body da requisição através do req.body
+const body = req.body
 
 //  Função para adicionar um novo item na lista acessando a propriedade 'nome' do body
 const novoItem = body.nome
@@ -46,23 +47,34 @@ lista.push(novoItem)
 
 // Implementando Endpoint Update [PUT](atualizar informação completa) /personagem/:id
 app.put('/personagem/:id', function(req, res){
-  // Acessando o ID dos parâmetros de rota
-  const id = req.params.id
+// Acessando o ID dos parâmetros de rota
+const id = req.params.id
 
-  // Função para acessar o Body da requisiçao
-  const body = req.body
+// Função para acessar o Body da requisiçao
+const body = req.body
 
-  // Função para acessar a propriedade 'nome' do body
-  const novoItem = body.nome
+// Função para acessar a propriedade 'nome' do body
+const novoItem = body.nome
 
-  // Função para atualizar na lista novoItem pelo ID -1
-  lista[id - 1] = novoItem 
+// Função para atualizar na lista novoItem pelo ID -1
+lista[id - 1] = novoItem 
   
-  // Função para exibir a mensagem de sucesso
+// Função para exibir a mensagem de sucesso
   res.send('Item atualizado com sucesso: ' + id + ' - ' + novoItem)
-
 })
 
+// Implementação do Endpoint Delete [DELETE] /personagem/:id
+app.delete('/personagem/:id', function (req, res){
+
+// Funçãp para acessar o parâmetro de rota
+const id = req.params.id
+
+// remover o item da lista usando o ID -1
+delete lista[id-1]
+
+//Enviamos uma mensagem de sucesso
+  res.send('Item removido con sucesso: ' + id)
+})
 
 
 app.listen(3000)
